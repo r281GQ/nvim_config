7c302f2f0923af415fd6d96cbbced5eae4f8bfdb
Endre Vegh <github.com.guiding230@passmail.net>
2023-11-16 08:12:30 +0000

    🔧 Add zen mode and trouble.

--- plugins.lua
+++ plugins.lua
@@ -23,6 +23,25 @@ local plugins = {
       return require "custom.configs.cmp"
     end,
   },
+  -- Lua
+  {
+    "folke/zen-mode.nvim",
+    opts = {
+
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
   {
     "williamboman/mason.nvim",
     opts = overrides.mason,
